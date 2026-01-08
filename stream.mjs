ed5c57ac161bc3aaafeec73a16f2503b8d39c84a
annasnurdin <anasnurdin02@gmail.com>
2026-01-08 14:49:55 +0700

    stream

---
+++ stream.mjs
@@ -0,0 +1,12 @@
+import fs from "fs"
+
+const writer = fs.createWriteStream("target.log") //dia akan timpa filenya
+writer.write("Annas\n")
+writer.write("Nurdin")
+writer.end()
+
+const render = fs.createReadStream("target.log")
+render.on("data", function(data){
+  console.info(data.toString())
+  render.close
+})
